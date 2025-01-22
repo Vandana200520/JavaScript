38dc74dcb527acbdbc272e2b6782d28cabd8b8d9
Vandana200520 <157909108+Vandana200520@users.noreply.github.com>
2025-01-22 23:13:06 +0530

    01_functions.js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -51,6 +51,9 @@ function loginUserMessage(username = "pammy"){   //it means that, if user inputs
 // console.log(loginUserMessage("vandana"))
 // console.log(loginUserMessage(""))
 
+
+//Shopping carts - when we dont know the number of enteries, but we have to store them
+
 // function calcCardPrice( num1 ){
 //     return num1
 // }
@@ -58,31 +61,40 @@ function loginUserMessage(username = "pammy"){   //it means that, if user inputs
 
 // rest operator ... ( to collect in array)
 
-function calcCardPrice( ...num1 ){       // or ( val1 , val2, ...num1)
+function calcCardPrice( ...num1 ){
     return num1
 }
-console.log( calcCardPrice(200 , 400, 2000,600));
+console.log(calcCardPrice(200 , 400, 2000, 600));  //op - [200, 400, 2000, 600]
+
+// function calcCardPrice(val1, val2, ...num1 ){   // val1 - 200, val2 - 400, and left are in num1
+//     return num1
+// }
+// console.log(calcCardPrice(200 , 400, 2000, 600));  //op - [2000, 600]
 
 const user = {
     username: "vandana",
     prices: 199
 }
 
-function handleObject(anyobject){
+function handleObject(anyobject){    //   we can use this function for any object
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-// handleObject(user)
-handleObject({
+// handleObject(user)            // call the function and pass an object
+
+handleObject({               //here we are directly passing the object
     username: "sam",
     price: 399
 })
 
 const myNewArray = [200, 400, 100, 600]
 
+//this funcion will get array as an input
+
 function returnSecondValue(getArray){
     return getArray[1]
 }
 
-// console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+// console.log(returnSecondValue(myNewArray));             // passing the array, thats declared in variable
+console.log(returnSecondValue([200, 400, 500, 1000]));     // directly passing the array as input
+ 

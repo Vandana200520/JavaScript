ddcba16ef40d767fb0e1d706a0406c330d1d2925
Vandana200520 <157909108+Vandana200520@users.noreply.github.com>
2025-01-23 23:50:20 +0530

    03_arrow.js

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -1,5 +1,6 @@
 // this - refer to current context
-// its used in objects. and can't use in functions
+// its used in objects. 
+// but can't use in functions (it will show undefined)
 
 const user = {
     username: "Vandana",
@@ -7,35 +8,40 @@ const user = {
 
     welcomeMessage: function() {
         console.log(` ${ this.username} , welcome to website.`);
-        // console.log(this);
+        // console.log(this);     //op - { username: 'Vandana', price: 999, welcomeMessage: [Function: welcomeMessage] }
     }
 }
 
-user.welcomeMessage()
+user.welcomeMessage()       //op - Vandana , welcome to website.
 user.username = "Gera"
-user.welcomeMessage()
+user.welcomeMessage()       //op - Gera , welcome to website.
 
 console.log(this);              //empty {}
 // in browser - Windows
 
 
-/*
+/*   //simple function
 function abc(){
     let username = "vandana"
-    console.log(this.username);    //undefined                   
+    console.log(this.username);    //undefined
+    
+    // console.log(this);   //op - <ref *1> Object [global] {}  // so much funs and variables
+    
 }
 abc()
 */
 
-/*
+/*   //holding in a variable
 const abc = function (){
     let username = "vandana"
-    console.log(this.username);    //undefined                   
+    console.log(this.username);    //undefined 
+
+    // console.log(this);   //op - <ref *1> Object [global] {}  // so much funs and variables
 }
 abc()
 */
 
-/*
+/*   //arrow function
 const abc = () => {
     let username = "vandana"
     console.log(this.username);    //undefined    
@@ -46,24 +52,25 @@ abc()
 
 /********** Arrow Function **********/
 
-/*  //basic arrow fun 
+/*  //basic arrow fun      // using curly braces - then uh need to write return keyword
+// if using parentesis - then no need to write return keyword
 
 const addTwo = ( num1,num2) => {
-    return num1+num2
+    return num1 + num2
 }
 console.log(addTwo(9,4));
 */
 
-// impicit return
+// impicit return 
 
-//const addTwo = ( num1,num2) => num1+num2
+//const addTwo = ( num1,num2) => num1 + num2         //both are same, with() or without(), just remember{}
+// const addTwo = ( num1,num2) => (num1 + num2)
 
-// const addTwo = ( num1,num2) => (num1+num2)
 
+const addTwo = (num1,num2) => ({username:"vandu"})         //wrap-up object in parenthesis to return to op 
+// const addTwo = (num1,num2) => {return {username:"vandu"}}  //both ways are correct
 
-const addTwo = ( num1,num2) => ({username:"vandu"})
-
-console.log(addTwo(9,4));
+console.log(addTwo(9,4));    //op - { username: 'vandu' }
 
 const myArray = [ 2, 5, 7, 3]
 

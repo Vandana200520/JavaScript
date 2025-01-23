fd49a5fb4608de74276dcd31187e3969eea9d7d5
Vandana200520 <157909108+Vandana200520@users.noreply.github.com>
2025-01-24 00:08:18 +0530

    04_iife.js

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -8,16 +8,21 @@ Immediately Invoked Function Expressions ( IIFE )
 
 /*
 function start(){
-    //named IIFE
+    //named IIFE   //start
     console.log(` DB connected `);
 }
 start();                          //required semi-colon
 */
 
 // SYNTAX --------- (fun definition) (execution)
+// ()()
+// fst parenthesis, in which function definition will be written
+// 2nd parenthesis, like to run or for the execution of function, it can take the parameters also, like we give in function calls
+
+
 // (function start(){
 //     console.log(` DB connected `);
-// })();                         //required semi-colon
+// })();                         //required semi-colon, cuz it doesn't know where to end the execution
 
 /*
 // ++++++++ Arrow function
